67c647a62a2fe40622caae3d3d63f759300d716a
lzy <2397925443@qq.com>
2022-07-10 17:04:40 +0800

    ci: fix jest config and add vue test

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,12 @@ export default {
   coverageDirectory: 'coverage',
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.vue$': 'vue3-jest'
+  },
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
   }
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,9 @@
 import foo from './foo'
+import { mount } from '@vue/test-utils'
+import Hello from '@/components/HelloWorld.vue'
 describe('init', () => {
   test('sum', () => {
+    console.log(mount(Hello))
     console.log(foo())
     expect(1 + 2).toBe(3)
   })
